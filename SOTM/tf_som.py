0f9000065952c740dfd328960e4d70244f752f73
Lisa-dk <60975431+Lisa-dk@users.noreply.github.com>
2022-07-14 20:19:50 +0200

    Update tf_som.py

--- SOTM/tf_som.py
+++ SOTM/tf_som.py
@@ -1,6 +1,6 @@
 """
 This code is adapted from https://github.com/cgorman/tensorflow-som/blob/tfv2/tf_som.p 
-such that is can be used to train a codebook with the SOTM method.
+such that it can be used to train a codebook with the SOTM method.
 
 Changes were made such that a SOM can be initialised using given weights. 
 Lines 214 and 215 were added, and the function  _prev_weights_init in line 450.
